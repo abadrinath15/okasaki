0cae3e7464dbb4b4b4a9d475191942229115d445
Aditya Badrinath <abadrinath15@gmail.com>
2023-06-07 16:12:09 -0600

    i'm in shock this is correct?

--- tests/test_structures/test_stream.py
+++ tests/test_structures/test_stream.py
@@ -1,10 +1,19 @@
-from okasaki.structures.stream import take, cons, equals
+from okasaki.structures.stream import take, cons, equals, append
 from okasaki.defer import delay_literal, force
+
+S = cons(1, cons(2, cons(3, delay_literal(None))))
+T = cons(4, cons(5, cons(6, delay_literal(None))))
 
 
 def test_take() -> None:
-    s = cons(1, cons(2, cons(3, delay_literal(None))))
-    assert force(take(0, s)) is None
-    test_res = take(2, s)
-    expected = cons(1, cons(2, delay_literal(None)))
-    assert equals(test_res, expected)
+    assert force(take(0, T)) is None
+    res = take(2, S)
+    ex = cons(1, cons(2, delay_literal(None)))
+    assert equals(res, ex)
+
+
+def test_append() -> None:
+    assert equals(append(delay_literal(None), T), T)
+    ex = cons(1, cons(2, cons(3, cons(4, cons(5, cons(6, delay_literal(None)))))))
+    res = append(S, T)
+    assert equals(res, ex)
